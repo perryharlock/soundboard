5b2f9c399f88d490ef11cedef1bb56a3a2af7673
perryharlock <perry.harlock@tiscali.co.uk>
2013-01-04 16:24:07 +0000

    Updated images and css

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	$("audio").removeAttr("controls").each(function(i, audioElement) {
 	    var audio = $(this);
 	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'">'+audio.attr("title")+'</a></li>').click(function() {that.play();}));
+	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {that.play();}));
 	});
 
 });
